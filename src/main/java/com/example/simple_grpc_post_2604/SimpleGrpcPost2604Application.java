3fff151cf21ed1ea67631c4f1026593bf908a208
github-insu <93insu@gmail.com>
2026-04-30 00:20:43 +0900

    chore: first commit

---
+++ src/main/java/com/example/simple_grpc_post_2604/SimpleGrpcPost2604Application.java
@@ -0,0 +1,13 @@
+package com.example.simple_grpc_post_2604;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleGrpcPost2604Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleGrpcPost2604Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/simple_grpc_post_2604/SimpleGrpcPost2604ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.simple_grpc_post_2604;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleGrpcPost2604ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
